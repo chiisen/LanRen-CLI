f8e7306f132a6a134bb867e412a27adcc6124c1c
chiisen <chiisen@hotmail.com>
2022-01-15 08:17:18 +0800

    feat: 新增說明

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # LanRen-CLI
-LanRen CLI
+Lan(懶)Ren(人) CLI
 
+# 安裝 commander
+```
+npm install commander --save
+```
+
+# 安裝 LanRen-CLI
+```
+npm link
+```
+這個指令將會幫助你把這個資料夾放進你的全域node module 中，這樣妳就不用部屬到 npm 上才能使用。
+
+# 解安裝 
+```
+npm unlink LanRen-CLI
+
+
+npm rm --global LanRen-CLI
+
+npm ls --global LanRen-CLI
+```
+
+# 查看版本號
+```
+lr -V
+or
+lr --version
+```
+
+# 查看說明
+```
+lr -h
+or
+lr --help
+```
--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+const { inspect } = require("util")
+
+const program = require("commander")
 
 program
-  .version('0.1.0')
-  .option('-s, --spicy', '要辣辣的, Add spicy')
-  .option('-P, --no-parsley', '不要香菜, Remove parsley')
-  .option('-t, --teatype [teatype]', '給我一杯 [紅茶]', '紅茶')
-  .parse(process.argv);
+  .version("0.0.1")
+  .option("-s | --spicy <on>", "spicy, Add spicy") // <變數> 會被視作參數
+  .option("-P | --no-parsley", "no parsley, Remove parsley") // no- 開頭會預設 parsley 為 true
+  .option("-t | --teatype [teatype]", "給我一杯 [teatype]", "紅茶") // 可以沒有參數，最後的'紅茶'是預設值
+  .option("-c | --cc <cctype>", "cc <cctype>") // 一定要填參數
+  .parse()
 
-console.log('you ordered:');
-if (program.spicy)
-  console.log('要辣辣');
-else
-  console.log('不要辣辣');
+const opts = program.opts()
+console.log("command option: " + inspect(opts))
 
-if (program.parsley)
-  console.log('我要香菜');
-else
-  console.log('我不要香菜');
-console.log('給我一杯 %s', program.teatype);
+for (const key in opts) {
+  console.log(`command type: ${key} ${program.getOptionValue(key)}`)
+}
+
+if (opts.spicy) console.log("spicy")
+
+if (opts.parsley) console.log("parsley")
+
+if (opts.teatype) {
+  console.log("給我一杯 %s", opts.teatype)
+} else {
+  console.log("謝謝!我不渴唷!")
+}
+
+if (opts.cc) {
+  console.log("cc %s", opts.cc)
+} else {
+  console.log("謝謝!cc!")
+}
