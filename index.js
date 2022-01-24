4b899792eb903f58296b996d575c66f28baefde8
chiisen <chiisen@hotmail.com>
2022-01-25 03:05:56 +0800

    調整 debug 訊息內容

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const program = require("commander")
 
 program
   .version("0.0.3")
-  .option("-d | --no-default", "no default, Remove default") // no- 開頭會預設 default 為 true
-  .option("-demo | --demo [demoString]", "demo [demoString]", `default`) // 可以不填 demoString ，預設為 default
+  .option("-d | --no-non_debug", "是否不顯示 debug 資訊") // --no- 開頭會預設 non_debug 為 true
+  .option("-o | --option_type [option_type]", "顯示參數內容的格式 [option_type]", 0) // 可以不填 option_type ，預設為 0
   .option("-e | --rsa_encrypt <decryptString>", "RSA 解密加密字串，須配合 private.pem")
   .option("-c | --rsa_create <dc>", "產生指定 dc 的 RSA public/private key 檔案")
   .option("-s | --dcsetting_common <dc>", "新增通用型單錢包的 dc_setting")
   .option("-u | --dcsetting_update_endpoint <dc>", "更新 dc_setting 的 endpoint")
   .parse()
 
-// 是否顯示參數內容
-const isDumpOption = false
-const dumpOptionType = 1
-
 const opts = program.opts()
 
+console.log(`是否不顯示 debug 資訊: non_debug = ${opts.non_debug} ` + (opts.non_debug ? "(不顯示)" : "(顯示)"))
+
 // @note 印出 option 的參數
-if (isDumpOption) {
-  if (dumpOptionType === 1) {
+if (!opts.non_debug) {
+  // 顯示參數內容的格式
+  console.log(`顯示參數內容的格式: opts.option_type = ${opts.option_type} `)
+  if (opts.option_type === 0) {
     console.log("command option: " + inspect(opts)) // 全部一行
   } else {
     for (const key in opts) {
@@ -36,9 +36,7 @@ if (isDumpOption) {
   }
 }
 
-console.log(`default = ${opts.default}`)
-console.log(`encrypt = ${opts.rsa_encrypt}`)
-if (opts.rsa_encrypt && opts.default) {
+if (opts.rsa_encrypt) {
   console.log(`\n 開始 RSA 解密`)
   // `XURMCTI2iufOI1AeyIKJmn8RmOEiEZAlUJHAw+RrwE0Yb/0OswwBA6yviW6Yd6dePkSL7Q1hsgCfdRFcpPKIjvzIyNs58Hi4Ib7QtHl5qLuQUJVqenohEsYx7PWV4gbC5GPRB8uDkLJCbnDXQr1iRU9/nUgHIGBq8x5rBKjE1ejnovSEETgVGh8+HbLpMC36jJfS4dwRT7aatZ96AJED+COuAFC8icnjcssQEBnQg675wDo7vi5UrXLac8tLxEQwg61VMYjiC63xw19Vf2W/ueR3Es9B5lStREFqdf9hD5mHkjuMYWIMNhsKm60cZ48WqiArwwZsFWsINCUcDJD8WEBWqfocEVnH3nAg9GIfnIwsjEqHZxjmyatvVkF0PYwOZD8NqhWh2sv0jBpR/8WP+rwfuMVKoWdJlU6qmySHlAtektX5CHvUH2lEZh4r8xL6CrwveVi56f0O9EBaSaAUNebTgFr5jNLiJH0URqApTOHdHzqWHdaMtaebNcaTjVKKm+fEG9bFICI5r2tNDwq9tg0xFl8660mchZM1TuHkA1FDoKMVTRWbmeT9Gpgrc6dCbdOxAFyMOxktwZ706jrZuruZmnGonkd1uz1BpVoJQgYKxNTY61ObwRvc4T8BHMGoijyp7KCJh2XBSpTas4dqgtfVQtHqZ1Bu6L/Z2Tj+nNhRFc/xpqCYmzVBZKOUkivj1+BgcOClSEu4A+E1lopgvfOVLN2LYnlylgQfC77BvcPtLxoCLgCOLKbYR6+vYvUO5jJooIrVgm5guuYbKHv5gpJpsHrIVlbd0vnSgA9gBNsvzuLJmkoS+MigPGV7IouVvUVccTbd7cIqTMWqK/w+BK8R79zj4cX2OvtoPiANLb2wwM7cmv9z9/AY+HRNdNzxgTZ/fVIa0s9CKl1o27FZprcHPcwdiYAvU+EvOAEHS1WAnbmWO1c/SenZjkdCME92q+xNy/CYM7JDrDEVRcmCM8/5EhXFgE0h3oeNGYs/CJhq9ZMcZEVISyAu184GB7DB`
   // 上面可測試解密
