38e1a9e416519e2bb5010c75d1e43536b873e429
chiisen <chiisen@hotmail.com>
2022-03-04 07:44:42 +0800

    fix: 新增幣別修正初始化

--- template/add_denom.js
+++ template/add_denom.js
@@ -5,12 +5,90 @@ const { parse } = require("csv-parse")
 const { warnColor, successColor } = require("../color/color")
 
 /**
+ * 讀取 allDenom.csv
+ * @param {*} opts
+ */
+function readcsv() {
+  const denomListCsv = `denomListCsv.csv`
+  if (!fs.existsSync(denomListCsv)) {
+    console.error(`\n 讀檔失敗，找不到 ${denomListCsv}`)
+    process.exit(1)
+  }
+
+  const rootPath = `add_denom`
+  const path = `./${rootPath}`
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path)
+  }
+  const denom = "denomList"
+  let subPath = `${path}/${denom}`
+  let count = 0
+  do {
+    if (!fs.existsSync(subPath)) {
+      fs.mkdirSync(subPath)
+    } else {
+      count += 1
+      subPath += `-${count}`
+      continue
+    }
+  } while (!fs.existsSync(subPath))
+
+  console.log(subPath)
+
+  README(fs, subPath)
+
+  fs.readFile(`./${denomListCsv}`, async (err, data) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(clc.cyan("csv-parse start"))
+
+    const csvData = []
+    fs.createReadStream(`./${denomListCsv}`)
+      .pipe(parse({ delimiter: ":" }))
+      .on("data", function (csvrow) {
+        //do something with csvrow
+        csvData.push(csvrow)
+        console.log(csvrow)
+      })
+      .on("end", function () {
+        //do something with csvData
+        console.log(clc.cyan("csv-parse end"))
+
+        console.log(successColor(`新增多筆【遊戲幣別】完成!`))
+      })
+  })
+
+  console.error(clc.red(`readcsv`))
+}
+
+/**
+ * 寫入 README
+ * @param {*} opts
+ */
+function README(fs, subPath) {
+  fs.writeFileSync(
+    `${subPath}/README.md`,
+    `請確定在後台有新增【遊戲幣別】與設定【匯率】
+後台操作可以參考 http://jira.i8.games/confluence/pages/viewpage.action?pageId=27590719
+
+請 IT 執行 alter.sql`,
+    "utf8"
+  )
+}
+
+/**
  * 新增幣別
  *
  * @param {*} opts[0] denom
  */
 function add_denom(opts) {
   const denom = opts[0]
+  if (denom === "readcsv") {
+    readcsv()
+    process.exit(1)
+  }
   if (denom == undefined) {
     console.error(`請輸入新增幣別`)
     process.exit(1)
@@ -39,14 +117,11 @@ function add_denom(opts) {
     fs.createReadStream(`./${denomCsv}`)
       .pipe(parse({ delimiter: ":" }))
       .on("data", function (csvrow) {
-        //console.log(csvrow)
         //do something with csvrow
         csvData.push(csvrow)
       })
       .on("end", function () {
         //do something with csvData
-        //console.log(csvData)
-
         console.log(clc.cyan("csv-parse end"))
 
         const rootPath = `add_denom`
@@ -67,19 +142,12 @@ function add_denom(opts) {
           }
         } while (!fs.existsSync(subPath))
 
-        fs.writeFileSync(
-          `${subPath}/README.md`,
-          `請確定在後台有新增【遊戲幣別】與設定【匯率】
-      後台操作可以參考 http://jira.i8.games/confluence/pages/viewpage.action?pageId=27590719
-      
-      請 IT 執行 alter.sql`,
-          "utf8"
-        )
+        README(fs, subPath)
 
         const nextLine = "\r\n"
         fs.writeFileSync(
           `${subPath}/alter.sql`,
-          `INSERT INTO \`game\`.\`game_default_currency_denom\` (\`Currency\`,\`Denom\`) VALUES ('${denom}',"13,12,11,10,9,8,7,6,5,4")
+          `INSERT INTO \`game\`.\`game_default_currency_denom\` (\`Currency\`,\`Denom\`) VALUES ('${denom}',"${indexList}")
 ON DUPLICATE KEY UPDATE \`Denom\` = VALUES(\`Denom\`);` +
             nextLine +
             nextLine,
